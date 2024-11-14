2c3bfbe56a52d6bd3889ffe3ae6c43c737d4b2c0
leomerida15 <dimasmerida15@gmail.com>
2024-11-14 03:12:26 -0400

    0.2.5

--- src/useSupabase/query/createSupabaseQuery.tsx
+++ src/useSupabase/query/createSupabaseQuery.tsx
@@ -1,116 +1,136 @@
-import { PostgrestError, SupabaseClient } from '@supabase/supabase-js';
-import { useInfiniteQuery, useQuery, UseQueryOptions } from '@tanstack/react-query';
-import { useMemo } from 'react';
-import { QueryBuilder } from './build';
-import { DatabaseTemp, SupabaseQueryResult } from './types';
-import { SupabaseInfoniteQueryConfig } from './types.infonite';
-import { SupabaseQueryConfig } from './types.query';
+import { PostgrestError, SupabaseClient } from "@supabase/supabase-js";
+import {
+  useInfiniteQuery,
+  useQuery,
+  UseQueryOptions,
+} from "@tanstack/react-query";
+import { useMemo } from "react";
+import { QueryBuilder } from "./build";
+import { DatabaseTemp, SupabaseQueryResult } from "./types";
+import { SupabaseInfoniteQueryConfig } from "./types.infonite";
+import { SupabaseQueryConfig } from "./types.query";
 
-export const createSupabaseQuery = <D extends DatabaseTemp>(client: SupabaseClient<D>) => {
-    const useSupaQuery = <Rs = undefined | any,>({
-        table,
-        column = '*',
-        count,
-        options = {},
-        single,
-        enabled,
-        ...configObj
-    }: SupabaseQueryConfig<D>) => {
-        type V = Rs extends undefined
-            ? typeof single extends true
-                ? (D['public']['Tables'] & D['public']['Views'])[typeof table] extends {
-                      Row: infer R;
-                  }
-                    ? R
-                    : never
-                : (D['public']['Tables'] & D['public']['Views'])[typeof table] extends {
-                      Row: infer R;
-                  }
-                ? R[]
-                : never[]
-            : Rs;
-
-        const fetchData = async (signal: AbortSignal): Promise<SupabaseQueryResult<V>> => {
-            const QueryBase = client.from(table).select(column, { count });
-
-            const QueryFn = QueryBuilder<D>(configObj, QueryBase);
-
-            const { data, error, count: rowCount } = await QueryFn.abortSignal(signal);
-
-            if (error) throw error;
-
-            return {
-                count: rowCount ?? 0,
-                payload: data as V,
-            };
-        };
-
-        const initialData = useMemo(() => {
-            if (single) return { payload: {}, count: 0 } as SupabaseQueryResult<V>;
-            //
-            return { payload: [], count: 0 } as SupabaseQueryResult<V>;
-        }, [single]);
-
-        const { queryKey = [], ...optionsHooks } = options;
-
-        return useQuery<SupabaseQueryResult<V>, PostgrestError>({
-            queryKey: [
-                [table, ...queryKey].join('_'),
-                configObj.where,
-                configObj.limit,
-                single,
-                count,
-            ],
-            initialData,
-            queryFn: ({ signal }) => fetchData(signal),
-            ...(optionsHooks as Omit<
-                UseQueryOptions<SupabaseQueryResult<V>, PostgrestError>,
-                'queryKey' | 'queryFn'
-            >),
-        });
-    };
-
-    const useSupaInfiniteQuery = ({
-        table,
-        column = '*',
-        options,
-        enabled,
-        count = 'exact',
-        ...configObj
-    }: SupabaseInfoniteQueryConfig<D>) => {
-        type V = (D['public']['Tables'] & D['public']['Views'])[typeof table] extends {
+export const createSupabaseQuery = <D extends DatabaseTemp>(
+  client: SupabaseClient<D>
+) => {
+  const useSupaQuery = <Rs = undefined | any,>({
+    table,
+    column = "*",
+    count,
+    options = {},
+    single,
+    enabled,
+    ...configObj
+  }: SupabaseQueryConfig<D>) => {
+    type V = Rs extends undefined
+      ? typeof single extends true
+        ? (D["public"]["Tables"] & D["public"]["Views"])[typeof table] extends {
             Row: infer R;
-        }
-            ? R[]
-            : never[];
-        const fetchData = async (signal: AbortSignal): Promise<SupabaseQueryResult<V>> => {
-            const QueryBase = client.from(table).select(column, { count });
+          }
+          ? R
+          : never
+        : (D["public"]["Tables"] & D["public"]["Views"])[typeof table] extends {
+            Row: infer R;
+          }
+        ? R[]
+        : never[]
+      : Rs;
 
-            const QueryFn = QueryBuilder<D>(configObj, QueryBase);
+    const fetchData = async (
+      signal: AbortSignal
+    ): Promise<SupabaseQueryResult<V>> => {
+      const QueryBase = client.from(table).select(column, { count });
 
-            const { data, error, count: rowCount } = await QueryFn.abortSignal(signal);
+      const QueryFn = QueryBuilder<D>(configObj, QueryBase);
 
-            if (error) throw error;
+      const {
+        data,
+        error,
+        count: rowCount,
+      } = await QueryFn.abortSignal(signal);
 
-            return {
-                count: rowCount ?? 0,
-                payload: data as V,
-            };
-        };
+      if (error) throw error;
 
-        const { initialData: InitProp, ...optionsHook } = options;
-
-        return useInfiniteQuery<SupabaseQueryResult<V>, PostgrestError>({
-            queryKey: [table, configObj.where, configObj.limit],
-            queryFn: ({ signal }) => fetchData(signal),
-            enabled,
-            ...optionsHook,
-        });
+      return {
+        count: rowCount ?? 0,
+        payload: data as V,
+      };
     };
 
-    return {
-        useSupaInfiniteQuery,
-        useSupaQuery,
-        QueryBuilder,
+    const initialData = useMemo(() => {
+      if (single) return { payload: {}, count: 0 } as SupabaseQueryResult<V>;
+      //
+      return { payload: [], count: 0 } as SupabaseQueryResult<V>;
+    }, [single]);
+
+    const { queryKey = [], ...optionsHooks } = options;
+
+    return useQuery<SupabaseQueryResult<V>, PostgrestError>({
+      queryKey: [
+        [table, ...queryKey].join("_"),
+        configObj.where,
+        configObj.limit,
+        single,
+        count,
+      ],
+      initialData,
+      queryFn: ({ signal }) => fetchData(signal),
+      enabled,
+      ...(optionsHooks as Omit<
+        UseQueryOptions<SupabaseQueryResult<V>, PostgrestError>,
+        "queryKey" | "queryFn"
+      >),
+    });
+  };
+
+  const useSupaInfiniteQuery = ({
+    table,
+    column = "*",
+    options,
+    enabled,
+    count = "exact",
+    ...configObj
+  }: SupabaseInfoniteQueryConfig<D>) => {
+    type V = (D["public"]["Tables"] &
+      D["public"]["Views"])[typeof table] extends {
+      Row: infer R;
+    }
+      ? R[]
+      : never[];
+    const fetchData = async (
+      signal: AbortSignal
+    ): Promise<SupabaseQueryResult<V>> => {
+      const QueryBase = client.from(table).select(column, { count });
+
+      const QueryFn = QueryBuilder<D>(configObj, QueryBase);
+
+      const {
+        data,
+        error,
+        count: rowCount,
+      } = await QueryFn.abortSignal(signal);
+
+      if (error) throw error;
+
+      return {
+        count: rowCount ?? 0,
+        payload: data as V,
+      };
     };
+
+    const { initialData: InitProp, ...optionsHook } = options;
+
+    return useInfiniteQuery<SupabaseQueryResult<V>, PostgrestError>({
+      queryKey: [table, configObj.where, configObj.limit],
+      queryFn: ({ signal }) => fetchData(signal),
+      enabled,
+      ...optionsHook,
+    });
+  };
+
+  return {
+    useSupaInfiniteQuery,
+    useSupaQuery,
+    QueryBuilder,
+  };
 };
--- src/useSupabase/subscription/createSupabaseSubscription.tsx
+++ src/useSupabase/subscription/createSupabaseSubscription.tsx
@@ -1,135 +1,141 @@
 import {
-    REALTIME_LISTEN_TYPES,
-    REALTIME_POSTGRES_CHANGES_LISTEN_EVENT,
-    SupabaseClient,
-} from '@supabase/supabase-js';
-import { QueryKey, useQueryClient } from '@tanstack/react-query';
-import { useEffect, useMemo } from 'react';
-import { createSupabaseQuery, DatabaseTemp } from '../query';
-import { PayloadRealtime, SupaSubscriptionProps } from './types';
+  REALTIME_POSTGRES_CHANGES_LISTEN_EVENT,
+  SupabaseClient,
+} from "@supabase/supabase-js";
+import { QueryKey, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useMemo } from "react";
+import { createSupabaseQuery, DatabaseTemp } from "../query";
+import { PayloadRealtime, SupaSubscriptionProps } from "./types";
 
 export const createSupabaseSubscription = <D extends DatabaseTemp>(
-    client: SupabaseClient<D>,
-    useQuery: ReturnType<typeof createSupabaseQuery<D>>['useSupaQuery'],
+  client: SupabaseClient<D>,
+  useQuery: ReturnType<typeof createSupabaseQuery<D>>["useSupaQuery"]
 ) => {
-    const useSupaSubscription = ({
+  const useSupaSubscription = ({
+    table,
+    schema = "public",
+    event = "*",
+    where,
+    type = "postgres_changes",
+    channel = "general",
+    callback = (payload) => console.log(payload),
+  }: SupaSubscriptionProps<D>) => {
+    const filter = useMemo(() => {
+      if (!where) return "";
+      const base = `${where?.key}=${where?.operator}`;
+
+      if (where?.operator === "in")
+        return `${base}.(${where?.value.toString()})`;
+
+      return `${base}.${where?.value}`;
+    }, [where]);
+
+    const configQuery = useMemo(() => {
+      if (!filter)
+        return {
+          event,
+          schema,
+          table,
+        };
+
+      return {
+        event,
+        schema,
         table,
-        schema = 'public',
-        event = '*',
-        where,
-        type = 'postgres_changes',
-        channel = 'general',
-        callback = (payload) => console.log(payload),
-    }: SupaSubscriptionProps<D>) => {
-        const filter = useMemo(() => {
-            const base = `${where?.key}=${where?.operator}`;
+        filter,
+      };
+    }, [event, schema, table, filter]);
 
-            if (where?.operator === 'in') return `${base}.(${where?.value.toString()})`;
+    useEffect(() => {
+      const subscription = client
+        .channel(channel)
+        .on(type, configQuery, callback)
+        .subscribe();
 
-            return `${base}.${where?.value}`;
-        }, [where]);
+      return () => {
+        subscription.unsubscribe();
+      };
+    }, [callback, configQuery, type, channel]);
+  };
 
-        useEffect(() => {
-            const subscription = client
-                .channel(channel)
-                .on(
-                    type,
-                    {
-                        event,
-                        schema,
-                        table,
-                        filter,
-                    },
-                    callback,
-                )
-                .subscribe();
+  const useSupaRealtime = ({
+    table,
+    where,
+    channel = "general",
+  }: Omit<SupaSubscriptionProps<D>, "callback" | "type" | "event">) => {
+    const queryClient = useQueryClient();
 
-            return () => {
-                subscription.unsubscribe();
-            };
-        }, [filter, callback, event, schema, table, type, channel]);
-    };
+    const queryConfig = useMemo(
+      () => (where ? { [where.operator]: { [where.key]: where.value } } : {}),
+      [where]
+    );
 
-    const useSupaRealtime = ({
-        table,
-        where,
-        channel = 'general',
-    }: Omit<SupaSubscriptionProps<D>, 'callback' | 'type' | 'event'>) => {
-        const queryClient = useQueryClient();
+    const QueryKey = [
+      [table, "subscription"].join("_"),
+      queryConfig,
+      null, // limit
+      null, // single
+      "estimated", // count
+    ] as unknown as QueryKey;
 
-        const queryConfig = useMemo(() => (where ? { [where.key]: where.value } : {}), [where]);
+    const query = useQuery({
+      table,
+      where: queryConfig,
+      options: {
+        queryKey: ["subscription"],
+      },
+      count: "estimated",
+    });
 
-        const QueryKey = [table, 'subscription'].join('_') as unknown as QueryKey;
+    useSupaSubscription({
+      table,
+      schema: "public",
+      event: "*",
+      where,
+      type: "postgres_changes",
+      channel,
+      callback: (payload: PayloadRealtime<D, typeof table>) => {
+        const eventMatch = {
+          [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.DELETE]: () => {
+            queryClient.setQueryData(QueryKey, (oldData: any) => ({
+              ...oldData,
+              payload: oldData.payload.filter(
+                (d: any) => d.id !== payload.old.id
+              ),
+            }));
+          },
+          [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.INSERT]: () => {
+            queryClient.setQueryData(QueryKey, (oldData: any) => ({
+              ...oldData,
+              payload: [...oldData.payload, payload.new],
+            }));
+          },
+          [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE]: () => {
+            queryClient.setQueryData(QueryKey, (oldData: any) => {
+              const payload = oldData.payload.map((d: any) => {
+                if (d.id === payload.old.id) return payload.new;
 
-        const query = useQuery({
-            table,
-            where: queryConfig,
-            options: {
-                queryKey: ['subscription'],
-                staleTime: Infinity,
-            },
-        });
+                return d;
+              });
 
-        const filter = useMemo(() => {
-            const base = `${where?.key}=${where?.operator}`;
+              const count = payload.length;
+              return {
+                count,
+                payload,
+              };
+            });
+          },
+        };
 
-            if (where?.operator === 'in') return `${base}.(${where?.value.toString()})`;
+        eventMatch[payload.eventType]();
+      },
+    });
 
-            return `${base}.${where?.value}`;
-        }, [where]);
+    return query;
+  };
 
-        useEffect(() => {
-            const subscription = client
-                .channel(channel)
-                .on(
-                    REALTIME_LISTEN_TYPES.POSTGRES_CHANGES as 'system',
-                    {
-                        event: '*',
-                        schema: 'public',
-                        table,
-                        filter,
-                    },
-                    (payload: PayloadRealtime<D, typeof table>) => {
-                        const eventMatch = {
-                            [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.DELETE]: () => {
-                                queryClient.setQueryData(QueryKey, (oldData: any) => ({
-                                    ...oldData,
-                                    data: oldData.data.filter((d: any) => d.id !== payload.old.id),
-                                }));
-                            },
-                            [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.INSERT]: () => {
-                                queryClient.setQueryData(QueryKey, (oldData: any) => ({
-                                    ...oldData,
-                                    data: [...oldData.data, payload.new],
-                                }));
-                            },
-                            [REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE]: () => {
-                                queryClient.setQueryData(QueryKey, (oldData: any) => ({
-                                    ...oldData,
-                                    data: oldData.data.map((d: any) => {
-                                        if (d.id !== payload.old.id) return payload.new;
-
-                                        return d;
-                                    }),
-                                }));
-                            },
-                        };
-
-                        eventMatch[payload.eventType]();
-                    },
-                )
-                .subscribe();
-
-            return () => {
-                subscription.unsubscribe();
-            };
-        }, [queryClient, filter, channel, table, QueryKey]);
-
-        return query;
-    };
-
-    return {
-        useSupaSubscription,
-        useSupaRealtime,
-    };
+  return {
+    useSupaSubscription,
+    useSupaRealtime,
+  };
 };
